0cc35d6ad2630d3f85d93db975d8da3078639e25
butterfly1of4 <jhouck29@gmail.com>
2020-08-31 20:03:42 -0400

    css

--- front/src/components/Navbar/Navbar.js
+++ front/src/components/Navbar/Navbar.js
@@ -8,35 +8,35 @@ class Navbar extends Component {
   constructor() {
     super();
 
-    //  this.state ={classlist:'Navbar'}
+
   }
   render() {
     return (
       <nav className="NavBar">
-        <div className="Header">
+        <div className="Header ">
           <img src={CircleLogo} className="circleLogo" alt="circleLogo" />
           <ul className="Nav_item_wrapper">
-            <li className="navbar_home">
-              <Link className="Nav_Home_link" to="#">
+            <li className="navbar_item">
+              <Link className="Nav_link" to="#">
                 HOME
               </Link>
             </li>
-            <li className="navbar_portfolio">
-              <Link className="Nav_Portfolio_link" to="#">
+            <li className="navbar_item">
+              <Link className="Nav_link" to="#">
                 PORTFOLIO
               </Link>
             </li>
-            <li className="navbar_languages">
-              <Link className="Nav_Languages_link" to="#">
+            <li className="navbar_item">
+              <Link className="Nav_link" to="#">
                 LANGUAGES
               </Link>
             </li>
-            <li className="navbar_ABOUT">
-              <Link className="Nav_About_link" to="#">
+            <li className="navbar_item">
+              <Link className="NavAbout_link" to="#">
                 LANGUAGES
               </Link>
-            </li>    <li className="navbar_contact">
-              <Link className="Nav_Contact_link" to="#">
+            </li>    <li className="navbar_item">
+              <Link className="Nav_link" to="#">
                 LANGUAGES
               </Link>
             </li>
