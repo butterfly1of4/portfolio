cebe4ee2ea32c7dc2d7bad830b0b783ae8cb9dbc
butterfly1of4 <jhouck29@gmail.com>
2020-09-01 20:43:33 -0400

    resaving

--- front/src/components/Portfolio/Portfolio.js
+++ front/src/components/Portfolio/Portfolio.js
@@ -1,49 +1,51 @@
-import React, { Component } from 'react'
-import "./Portfolio.css"
+import React, { Component } from "react";
+import "./Portfolio.css";
 import "../../index.css";
 import { BrowserRouter as Router, Link } from "react-router-dom";
 
-
 // React Class Component
 class Portfolio extends Component {
-
-    render() {
-        return (
-            <div className="Portfolio">
-                <h1 className="portfolio_title">Portfolio.</h1>
-                <div className="project_thumbails">
-                    <div className="war">
-                        <div className="thumbnail_1">
-                        </div>
-                        <p>
-                            <Router>
-                                <Link className="github_war_link" to ="">GAME OF WAR</Link>
-                            </Router>
-                        </p>
-                    </div>
-                    <div className="nasa">
-                        <div className="thumbnail_2">
-                        </div>
-                        <p>
-                            <Router>
-                                <Link className="nasa_pages_link" to ="">NASA PLANETS PAGE</Link>
-                            </Router>
-                        </p>
-                    </div>    
-                    <div className="Nterflix">
-                        <div className="thumbnail_3">
-                        </div>
-                        <p>
-                            <Router>
-                                <Link className="nterflix_deployed_ink" to ="">NTERFLIX</Link>
-                            </Router>
-                        </p>
-                    </div>        
-                </div>
+  render() {
+    return (
+      <div className="Portfolio">
+        <h1 className="portfolio_title">Portfolio.</h1>
+        <div className="project container">
+          <div className="project_thumbails">
+            <div className="war">
+              <div className="thumbnail_1"></div>
+              <p>
+                <Router>
+                  <Link className="github_war_link" to="">
+                    GAME OF WAR
+                  </Link>
+                </Router>
+              </p>
             </div>
-        )
-    }
+            <div className="nasa">
+              <div className="thumbnail_2"></div>
+              <p>
+                <Router>
+                  <Link className="nasa_pages_link" to="">
+                    NASA PLANETS PAGE
+                  </Link>
+                </Router>
+              </p>
+            </div>
+            <div className="Nterflix">
+              <div className="thumbnail_3"></div>
+              <p>
+                <Router>
+                  <Link className="nterflix_deployed_ink" to="">
+                    NTERFLIX
+                  </Link>
+                </Router>
+              </p>
+            </div>
+          </div>
+        </div>
+      </div>
+    );
+  }
 }
 
-export default Portfolio
-
+export default Portfolio;
