57e33b494b12a3f67fd1ea3e772fba261392d710
butterfly1of4 <jhouck29@gmail.com>
2020-09-02 16:32:48 -0400

    added list

--- front/src/components/Languages/Languages.js
+++ front/src/components/Languages/Languages.js
@@ -4,21 +4,32 @@ import "./Languages.css";
 // Function based React Component
 const Languages = (props) => {
   return (
-    
     <div className="Languages">
       <h1 className="languages_title">Languages</h1>
       <p className="languages_wrapper">
         <ul className="languages_list">
+          <li className="languages_item">Django</li>
+          <div className="slashes">//</div>
+          <li className="languages_item">Python</li>
+          <div className="slashes">//</div>
+          <li className="languages_item">Node</li>
+          <div className="slashes">//</div>
+          <li className="languages_item">Express</li>
+          <div className="slashes">//</div>
+          <li className="languages_item">Git</li>
+          <div className="slashes">//</div>
+          <li className="languages_item">Back End</li>
+          <div className="slashes">//</div>
+          <li className="languages_item">MongoDB</li>
+          <div className="slashes">//</div>
+          <li className="languages_item">SQL</li>
+          <div className="slashes">//</div>
           <li className="languages_item"></li>
-          <li className="languages_item"></li>
-          <li className="languages_item"></li>
-          <li className="languages_item"></li>
-          <li className="languages_item"></li>
-          <li className="languages_item"></li>
-          <li className="languages_item"></li>
-          <li className="languages_item"></li>
-          <li className="languages_item"></li>
-          <li className="languages_item"></li> <li className="languages_item"></li> <li className="languages_item"></li>
+          <div className="slashes">//</div>
+          <li className="languages_item">Front End</li> <div className="slashes">//</div>
+          <li className="languages_item">CSS</li>
+          <li className="languages_item">HTML</li><div className="slashes">//</div>
+          <li className="languages_item">Javascript</li>
         </ul>
       </p>
     </div>
