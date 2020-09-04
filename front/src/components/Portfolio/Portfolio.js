cc3c40d6e57841b8fdd47f5c6aca48be4c4a6c91
butterfly1of4 <jhouck29@gmail.com>
2020-09-04 18:09:39 -0400

    updating

--- front/src/components/Portfolio/Portfolio.js
+++ front/src/components/Portfolio/Portfolio.js
@@ -1,6 +1,10 @@
 import React, { Component } from "react";
 import "./Portfolio.css";
 import "../../index.css";
+import BlackTank from "../../assets/BlackTank.png"
+import Saturn from "../../assets/Saturn.png"
+import FilmReel from "../../assets/FilmReel.png"
+
 import { Router, Link, Switch } from "react-router-dom";
 
 // React Class Component
@@ -8,14 +12,19 @@ class Portfolio extends Component {
   render() {
     return (
       <div className="Portfolio">
-        <h1 className="Portfolio_title">Portfolio<div className="Period">.</div></h1>
+        <h1 className="Portfolio_title">
+          Portfolio<div className="Period">.</div>
+        </h1>
         <div className="project_wrapper">
           <ul className="project_thumbnails">
             <li className="thumbnail_item">
-              <div className="thumbnail_Pic"></div>
+              <div className="thumbnail_Pic">
+
+                <img className="thumbnail_1"src={BlackTank} />
+              </div>
 
               <a
-                className="github_link"
+               style={{textDecoration:'none'}}
                 href="https://github.com/butterfly1of4/Game-of-War"
               >
                 GAME OF WAR
@@ -23,39 +32,38 @@ class Portfolio extends Component {
             </li>
 
             <li className="thumbnail_item">
-              <div className="thumbnail_Pic"></div>
+              <div className="thumbnail_Pic">            <img className="thumbnail_1"src={Saturn} /></div>
 
               <a
-                className="deployed_link"
+                className="deployed_link" style={{textDecoration:'none'}}
                 href="https://butterfly1of4.github.io/ui-pattern/"
               >
                 NASA PLANETS PAGE
               </a>
-              <a
-                className="github_link"
-                href="https://github.com/butterfly1of4/ui-pattern"
-              ></a>
+              <a className="ViewCode" style={{textDecoration:'none'}}href="https://github.com/butterfly1of4/ui-pattern">VIEW CODE</a>
             </li>
 
             <li className="thumbnail_item">
-              <div className="thumbnail_Pic"></div>
+              <div className="thumbnail_Pic">            <img className="thumbnail_1"src={FilmReel} /></div>
               <a
-                className="deployed_ink"
+                className="deployed_link" style={{textDecoration:'none'}}
                 href="https://butterfly1of4.github.io/project8-frontend"
               >
                 NTERFLIX
               </a>
               <a
-                className="github_link"
+                className="ViewCode"
+                style={{textDecoration:'none'}}
                 href="https://github.com/butterfly1of4/project8-frontend"
               >
-                FRONT END
+                VIEW CODE FOR FRONTEND
               </a>
               <a
-                className="github_link"
+                className="ViewCode"
+                style={{textDecoration:'none'}}
                 href="https://github.com/butterfly1of4/project8-backend"
               >
-                BACK END
+                VIEW CODE FOR BACKEND
               </a>
             </li>
           </ul>
