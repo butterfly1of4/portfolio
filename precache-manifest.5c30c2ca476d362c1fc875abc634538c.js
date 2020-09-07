81abf73e7cf3a629d4b2ac630ae8f5d2382b2c07
butterfly1of4 <jhouck29@gmail.com>
2020-09-07 09:45:39 -0400

    Updates

---
+++ precache-manifest.5c30c2ca476d362c1fc875abc634538c.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c82d48aa89749af40713ea8ddaf88fef",
+    "url": "/portfolio/index.html"
+  },
+  {
+    "revision": "13a18ff0d16478b6643b",
+    "url": "/portfolio/static/css/2.7f83aeb6.chunk.css"
+  },
+  {
+    "revision": "80f93036b2187678ca3c",
+    "url": "/portfolio/static/css/main.0b623475.chunk.css"
+  },
+  {
+    "revision": "13a18ff0d16478b6643b",
+    "url": "/portfolio/static/js/2.9db0f5fd.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/portfolio/static/js/2.9db0f5fd.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "80f93036b2187678ca3c",
+    "url": "/portfolio/static/js/main.bd30432c.chunk.js"
+  },
+  {
+    "revision": "4113d93a276f2b41f70f",
+    "url": "/portfolio/static/js/runtime-main.83eeea52.js"
+  },
+  {
+    "revision": "3f1247374b42926d19813411b2c35f48",
+    "url": "/portfolio/static/media/background.3f124737.png"
+  }
+]);
